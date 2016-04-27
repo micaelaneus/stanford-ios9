cf50714c4a848e295b39ec885fb465ed415e2834
Micaela Neus <micaelaneus@gmail.com>
2016-04-26 20:07:19 -0700

    Add display and append digits

--- StanfordCalc/CalculatorViewController.swift
+++ StanfordCalc/CalculatorViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+
+    @IBOutlet weak var display: UILabel!
+
     @IBAction func touchDigit(sender: UIButton) {
-        let digit = sender.currentTitle
-        print("Touched \(digit!) digit")
+        let digit = sender.currentTitle!
+        let currentDisplayText = display.text!
+        display.text = currentDisplayText + digit
     }
 }
 
